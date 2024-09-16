c52b2953951d0ae2c0d6564e293cc1b7540ca138
User <yuko.brandon13@gmail.com>
2024-09-16 16:27:26 +0500

    adding files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'
-import MainPage from './components/MainPage';
-import About from './components/About';
-import Contacts from './components/Contacts';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MainPage from "./components/MainPage";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
 
 function App() {
   return (
     <>
       <Router>
         <header>
-          <div className='span-div'>
+          <div className="span-div">
             <span>Оценка фильмов</span>
           </div>
           <ul>
@@ -23,13 +22,12 @@ function App() {
             <li>
               <Link to="/contacts"> Контакты</Link>
             </li>
-            
           </ul>
         </header>
         <Routes>
-          <Route path='/' element={<MainPage/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contacts' element={<Contacts/>}/>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
         </Routes>
       </Router>
     </>
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,35 +1,37 @@
-import "./mainpage.css"
+import "./mainpage.css";
 
-function Contacts(){
-    return(
-        <>
-            <div className="container-contacts">
-                <div>
-                    <h1>Контакты</h1>
-                </div>
-                <div>
-                    <span>Мы всегда рады услышать ваше мнение и помочь вам! <br/>
-                        Если у вас есть вопросы, предложения или вы хотите связаться с нами <br/>
-                        по любому поводу, не стесняйтесь использовать информацию ниже.
-                    </span>
-                </div>
-            </div>
-            
-            <div>
-                <form>
-                    <label htmlFor="name">Имя</label>
-                    <input id="name" className="name-input" type="text"/>
+function Contacts() {
+  return (
+    <>
+      <div className="container-contacts">
+        <div>
+          <h1>Контакты</h1>
+        </div>
+        <div>
+          <span>
+            Мы всегда рады услышать ваше мнение и помочь вам! <br />
+            Если у вас есть вопросы, предложения или вы хотите связаться с нами{" "}
+            <br />
+            по любому поводу, не стесняйтесь использовать информацию ниже.
+          </span>
+        </div>
+      </div>
 
-                    <label htmlFor="email">Почта</label>
-                    <input id="emaill" type="email"/>
+      <div>
+        <form>
+          <label htmlFor="name">Имя</label>
+          <input id="name" className="name-input" type="text" />
 
-                    <label htmlFor="message">Сообщение</label>
-                    <textarea id="message"></textarea>
+          <label htmlFor="email">Почта</label>
+          <input id="emaill" type="email" />
 
-                    <button type="submit">Отправить</button>
-                </form>
-            </div>
-        </>
-    )
+          <label htmlFor="message">Сообщение</label>
+          <textarea id="message"></textarea>
+
+          <button type="submit">Отправить</button>
+        </form>
+      </div>
+    </>
+  );
 }
-export default Contacts
+export default Contacts;
